96c472eed4927d754d5d750b6970faf6b22199b5
fanchangrui <1293707902@qq.com>
2021-09-16 15:40:35 +0800

    购物

--- components/w-goods-item/w-goods-item.js
+++ components/w-goods-item/w-goods-item.js
@@ -21,6 +21,13 @@ Component({
      * 组件的方法列表
      */
     methods: {
-
+        itemClick(e) {
+            // 1.获取iid
+            const iid = this.data.item.iid;
+            // 2.跳转到对应的路径
+            wx.navigateTo({
+              url: '/pages/detail/detail?iid=' + iid,
+            })
+          }
     }
 })
